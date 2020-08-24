67618eed37ccc972891e5d6228bc6a4ac0aa7a02
Elena Slyusareva <sportgirl55@bk.ru>
2020-08-25 01:33:41 +0500

    Second task completed, but first is not

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,41 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
-}
+    if (!isNaN(percent, contribution, amount, date)) {
+        console.log(`Параметр ${arguments} содержит неправильное значение ${arguments}`);
+    }     
+    let nowaday = new Date();
+    let totalMonths = (date.getFullYear() - nowaday.getFullYear()) * 12 + (date.getMonth() - nowaday.getMonth());
+    let creditBody = amount - contribution;
+    let monthlyPayment = creditBody * (((percent / 12) / 100) + ((percent / 12) / 100) / (((1 + ((percent / 12) / 100)) ** totalMonths) - 1));
+    let totalAmount = (monthlyPayment * monthlyPayment).toFixed(2);
+    return totalAmount;
+};
+
+
+
+
+// Ежемесячная оплата рассчитывается по формуле: Платеж=S*(P+P/(((1+P)^n)-1)), где: S - тело кредита, P - 1/12 процентной ставки (от 0 до 1), n - количество месяцев ^ - возведение в степень
+
+// Напишите функцию, которая будет принимать процентную ставку, сумму первоначального взноса, сумму кредита и срок (дату окончания кредита) в качестве аргументов и выдавать сумму, которую в итоге 
+// заплатит клиент (первоначальный взнос, погашение основного долга, проценты за пользование кредитом).
+
+// function calculateTotalMortgage(percent, contribution, amount, date) {
+//     if (arguments != Number)
+//     console.log(`Параметр <название параметра> содержит неправильное значение <значение параметра>`);
+//  let totalAmount = amount - contribution;
+//     return totalAmount;
+// }
+
+
+
+
+
+
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
-}
+    if ( !name || 0 === name.length) {
+        name = "Аноним";
+    }
+   let greeting = `Привет, мир! Меня зовут ${name}`;
+    return greeting;
+};
