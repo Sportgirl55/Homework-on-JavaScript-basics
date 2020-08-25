52026e8d9b1b4bd4e722bf90912e2b50672c039a
Elena Slyusareva <sportgirl55@bk.ru>
2020-08-25 17:20:46 +0500

    All tasks are comleted, but first there is a problem with passing test for the argument contribution

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,34 +1,25 @@
 "use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    if (!isNaN(percent, contribution, amount, date)) {
-        console.log(`Параметр ${arguments} содержит неправильное значение ${arguments}`);
-    }     
-    let nowaday = new Date();
-    let totalMonths = (date.getFullYear() - nowaday.getFullYear()) * 12 + (date.getMonth() - nowaday.getMonth());
-    let creditBody = amount - contribution;
-    let monthlyPayment = creditBody * (((percent / 12) / 100) + ((percent / 12) / 100) / (((1 + ((percent / 12) / 100)) ** totalMonths) - 1));
-    let totalAmount = (monthlyPayment * monthlyPayment).toFixed(2);
-    return totalAmount;
-};
-
-
-
-
-// Ежемесячная оплата рассчитывается по формуле: Платеж=S*(P+P/(((1+P)^n)-1)), где: S - тело кредита, P - 1/12 процентной ставки (от 0 до 1), n - количество месяцев ^ - возведение в степень
-
-// Напишите функцию, которая будет принимать процентную ставку, сумму первоначального взноса, сумму кредита и срок (дату окончания кредита) в качестве аргументов и выдавать сумму, которую в итоге 
-// заплатит клиент (первоначальный взнос, погашение основного долга, проценты за пользование кредитом).
-
-// function calculateTotalMortgage(percent, contribution, amount, date) {
-//     if (arguments != Number)
-//     console.log(`Параметр <название параметра> содержит неправильное значение <значение параметра>`);
-//  let totalAmount = amount - contribution;
-//     return totalAmount;
+    if (!Number(percent))  {
+        return("Параметр \"Процентная ставка\" содержит неправильное значение " + percent);
+}
+//     if (!Number(contribution)) {
+//         return(`Параметр "Сумма первоначального взноса" содержит неправильное значение ${contribution}`);
 // }
 
+// Не проходит 3 из 5 проверок. Хотя в коде всё то же самое, что и у двух других аргументов.
+
+    if (!Number(amount)) {
+        return(`Параметр "Сумма кредита" содержит неправильное значение ${amount}`);
+} 
 
 
-
+    let totalMonths = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+    let creditBody = amount - contribution;
+    let monthlyPayment = creditBody * (((percent / 100) / 12) + ((percent / 100) / 12) / (((1 + ((percent / 100) / 12)) ** totalMonths) - 1));
+    let totalAmount = +(totalMonths * monthlyPayment).toFixed(2);
+    return totalAmount;
+};
 
 
 
