e1837128a82c39b08d465c3e09d32694606df33b
Elena Slyusareva <sportgirl55@bk.ru>
2020-09-03 10:38:30 +0500

    All tasks are completed

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,39 @@
-//String.prototype.isPalindrome - для задачи №1
+"use strict"
+
+String.prototype.isPalindrome = function () {
+    let phrase = this.toUpperCase();
+    for (let i = 0; i <= phrase.length / 2; i++) {
+      if (phrase[i] === phrase[phrase.length - 1 - i]) {
+        return true;
+      } return false
+    }
+  };
+
+console.log("А роза упала на лапу Азора".isPalindrome());
+
+
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
-}
+    if (marks.length == 0) {
+        return 0;
+    };
+    let sumOfMarks = 0;
+        for (let i = 0; i < marks.length; i++) {
+        sumOfMarks += marks[i]; 
+    }
+    let average = sumOfMarks / marks.length;
+    let roundedAverage = Math.round(average);
+        return roundedAverage;    
+};
+  
+  
+
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
-}
+    let now = Date.now();
+    let diff = now - Number(new Date(birthday));
+    let age = diff / 31557600000;
+    let verdict = (age > 18 ? true : false);
+    return verdict;
+};
